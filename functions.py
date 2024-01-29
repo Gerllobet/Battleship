9d16111a929a7e570abee7ec9775040029b0979e
Gerllobet <137827945+Gerllobet@users.noreply.github.com>
2024-01-29 19:16:24 +0100

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,25 @@
+def startBoard():
+    row1 = ["W", "W", "W", "W", "W"]
+    row2 = ["W", "W", "W", "W", "W"]    
+    row3 = ["W", "W", "W", "W", "W"]    
+    row4 = ["W", "W", "W", "W", "W"]    
+    row5 = ["W", "W", "W", "W", "W"]    
+    board = [row1, row2, row3, row4, row5]
+    return(board)
+
+def showBoard(b):
+    for l in b:
+        print("".join(l))
+
+def final(b):
+    for l in b:
+        for c in l:
+            if(c == "S"):
+                return False
+    return(True)
+
+def thereIsNoNumber(s):
+    if(s[0] not in "01234" or s[-1] not in "01234"):
+        return True
+    else:
+        return False
